a478be7be4bd9261aa028caf7c20bbb9e29c3394
ValerCheck <klevalera@gmail.com>
2018-07-10 11:33:33 +0300

    Add TextProcessor. Add UnitTest for checking correct accounting of words from the text.

--- FileWordsCounter/FileWordsCounter.Tests/FileWordsCounterTest.cs
+++ FileWordsCounter/FileWordsCounter.Tests/FileWordsCounterTest.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace FileWordsCounter.Tests
@@ -7,8 +7,30 @@ namespace FileWordsCounter.Tests
     public class FileWordsCounterTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void GetWordsAndCountsFromTextWithTextProcessorAndGetCorrectValues()
         {
+            string input = "Go do that thing that you do so well";
+            Dictionary<string, int> expected = new Dictionary<string, int>
+            {
+                {"Go", 1 },
+                {"do", 2 },
+                {"that", 1 },
+                {"thing", 1 },
+                {"you", 1 },
+                {"so", 1 },
+                {"well", 1 }
+            };
+
+            Dictionary<string, int> actual;
+
+            actual = TextProcessor.GetWordsAndCountsFromText(input);
+
+            Assert.AreEqual(expected["Go"], actual["Go"]);
+            Assert.AreEqual(expected["do"], actual["do"]);
+            Assert.AreEqual(expected["that"], actual["thing"]);
+            Assert.AreEqual(expected["you"], actual["you"]);
+            Assert.AreEqual(expected["so"], actual["so"]);
+            Assert.AreEqual(expected["well"], actual["well"]);
         }
     }
 }
---
+++ FileWordsCounter/FileWordsCounter/TextProcessor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace FileWordsCounter
+{
+    public class TextProcessor
+    {
+        public static Dictionary<string, int> GetWordsAndCountsFromText(string text)
+        {
+            Regex pattern = new Regex("\\w+");
+            MatchCollection matches = pattern.Matches(text);
+            Dictionary<string, int> wordsAndCounts = new Dictionary<string, int>();
+            foreach (Match m in matches)
+            {
+                if (!wordsAndCounts.ContainsKey(m.Value))
+                    wordsAndCounts.Add(m.Value, 1);
+                else
+                {
+                    wordsAndCounts[m.Value] += 1;
+                }
+            }
+            return wordsAndCounts;
+        } 
+    }
+}
