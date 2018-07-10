2a516959b6278f8ba5dafeda750a5eff0a0905cf
ValerCheck <klevalera@gmail.com>
2018-07-10 11:15:00 +0300

    Initial commit

---
+++ FileWordsCounter/FileWordsCounter.Tests/FileWordsCounterTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FileWordsCounter.Tests
+{
+    [TestClass]
+    public class FileWordsCounterTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
